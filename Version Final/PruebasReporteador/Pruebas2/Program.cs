ac7f58b94aa6fdc4b3c7c22b4498fa84df0fd2da
VG1999 <esterciitaguamu@gmail.com>
2021-09-28 12:19:54 -0600

    codigo fuente Reporteador para sistema ERP

---
+++ Version Final/PruebasReporteador/Pruebas2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows.Forms;
+using CapaVistaReporteador;
+using CapaVistaReporteador.Mantenimientos;
+using CapaVistaReporteador.ReportesModulos;
+
+namespace Pruebas2
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new frmMenuReporteador());
+        }
+    }
+}
+//
